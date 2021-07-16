bf340892c7d6463173a4c2867b7bf005c88a8267
0xDrazilId <fga-as@mail.ru>
2021-07-16 21:52:08 +0400

    plus form php script

--- scripts/mail.php
+++ scripts/mail.php
@@ -1,23 +1,10 @@
 <?php
-$mail = "egor.feraf@gmail.com"; //to
-$headers = "From:fga-as@mail.ru";
-if(isset($_POST["submit"])) {
-        $message = "Новая заявка с сайта\nИмя: " . $_POST['name'] . "\nТелефон: " . $_POST['phone'];
+$mail = "Avtoservis-pioner.ru@yandex.ru";
+$headers = 'From: Avtoservis-pioner.ru';
+if ($_POST) {
+    if ($_POST['footer_form']) {
+        $message = "\nИмя: " . $_POST['zayavka_name'] . "\nТелефон: " . $_POST['zayavka_phone'] . "\nEmail: " . $_POST['zayavka_mail'];
         mail($mail, 'Новая заявка с сайта', $message, $headers);
+    }
 }
 header('location: /');
-
-// if(isset($_POST["submit"])) {
-//     $recipient = "egor.feraf@gmail.com"; //my email
-//     echo $subject = 'New Appeal';
-//     echo $name = $_POST ["name"];
-//     echo $phone = $_POST["phone"];
-    
-//      $mailBody="Name: $name\nphone: $phone\n\n"; 
-    
-//      mail($recipient, $subject, $mailBody, "From: $name <$email>");
-    
-    // echo "<p>Thank you! We will be in contact with you shortly.</p>";
-    
-    // }
-?>
